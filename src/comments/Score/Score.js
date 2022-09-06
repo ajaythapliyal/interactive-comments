10b5b39de0c12363fb9da2e8ccbb45014c9a10ab
ajaythapliyal <ajaythapliyal1703@gmail.com>
2022-09-06 08:49:18 +0530

    adds score component

---
+++ src/comments/Score/Score.js
@@ -0,0 +1,16 @@
+import styles from "./Score.module.css";
+import { ReactComponent as Minus } from "./../../assets/images/icon-minus.svg";
+import { ReactComponent as Plus } from "./../../assets/images/icon-plus.svg";
+
+export default function Score(props) {
+  return (
+    <div className={styles["score-container"]}>
+      <Plus onClick={props.increment} className={styles["score-action"]}></Plus>
+      <p className={styles.score}>{props.score}</p>
+      <Minus
+        onClick={props.decrement}
+        className={styles["score-action"]}
+      ></Minus>
+    </div>
+  );
+}
