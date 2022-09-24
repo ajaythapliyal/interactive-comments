25f7a0117cb4b0dd2470cf2e69d5aabdc3559dd2
ajaythapliyal <ajaythapliyal1703@gmail.com>
2022-09-24 21:53:37 +0530

    responsive comment section

--- src/comments/Comment/Comment.js
+++ src/comments/Comment/Comment.js
@@ -2,11 +2,18 @@ import styles from "./Comment.module.css";
 import Score from "./../Score/Score";
 import Reply from "./../Reply/Reply";
 import User from "./../User/User";
+import useMediaQuery from "../useMediaQuery";
 
 export function Comment(props) {
+  const isMobile = useMediaQuery("(max-width: 500px)");
+
   return (
     <div className={`${styles.bubble} ${styles.className}`}>
-      <Score className={styles.score} score={props.score}></Score>
+      <Score
+        className={styles.score}
+        score={props.score}
+        horizontal={isMobile}
+      ></Score>
       <User username={props.username} src={props.src}></User>
       <div className={styles.text}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
