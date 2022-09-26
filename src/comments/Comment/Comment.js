e382fc94d83bca16bda1c113558e86c6e7f0b602
ajaythapliyal <ajaythapliyal1703@gmail.com>
2022-09-26 08:04:13 +0530

    adds state in comment component and adds interactivity for like and dislike

--- src/comments/Comment/Comment.js
+++ src/comments/Comment/Comment.js
@@ -3,22 +3,29 @@ import Score from "./../Score/Score";
 import Reply from "./../Reply/Reply";
 import User from "./../User/User";
 import useMediaQuery from "../useMediaQuery";
+import { useState } from "react";
 
 export function Comment(props) {
+  const [score, setScore] = useState(props.score);
+  const [replies, setReplies] = useState(props.replies);
+
   const isMobile = useMediaQuery("(max-width: 500px)");
+
   return (
     <div className={styles["comment-section"]}>
       <div className={styles.bubble}>
         <Score
           className={styles.score}
-          score={props.score}
+          score={score}
           horizontal={isMobile}
+          increment={() => setScore(score + 1)}
+          decrement={() => setScore(score - 1)}
         ></Score>
         <User username={props.username} src={props.src}></User>
         <div className={styles.text}>{props.text}</div>
         <Reply className={styles.reply}></Reply>
       </div>
-      {props.replies?.map((reply) => (
+      {replies?.map((reply) => (
         <Comment
           key={reply.id}
           score={reply.score}
